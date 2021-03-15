3c73bf495b3274d1561f37b5f4dd93082359496d
sumeikom <sumeikom@gmail.com>
2021-03-15 15:33:00 -0400

    commit tree 5c8d75d39fa50f6dc877395c5f9b4d88e11c0fa0

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,19 @@
-class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  
+require 'pry'
 
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  
   def initialize(title)
     @title = title
+  end
+  
+  GENRES = []
+  
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre unless GENRES.any? { |g| g == genre }
   end
 
   def turn_page
